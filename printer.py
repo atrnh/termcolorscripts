ca25d277dce56d59bbbf5f39d8f56ca9b00e9621
Ashley Trinh <ashqtrinh@gmail.com>
2018-07-17 11:33:23 -0700

    Make printer a little smarter

--- printer.py
+++ printer.py
@@ -45,52 +45,54 @@ def print_line(line, width=None, real_w=None):
 
     print(s)
 
+if __name__ == '__main__':
+    import sys
 
-system('clear')
-print(SWIRL)
-blocks = make_blocks(4)
-width = 4 * 8
-print_line(' ' * SPACE_INSIDE)
-print_line(' G U M M Y')
-print_line(' ' * SPACE_INSIDE)
-print_line(''.join(blocks[:8]), width)
-print_line(''.join(blocks[:8]), width)
-print_line(''.join(blocks[:8]), width)
-print_line(''.join(blocks[:8]), width)
-print_line(''.join(blocks[:8]), width)
-print_line(''.join(blocks[8:]), width)
-print_line(''.join(blocks[8:]), width)
+    system('clear')
+    print(SWIRL)
+    blocks = make_blocks(4)
+    width = 4 * 8
+    print_line(' ' * SPACE_INSIDE)
+    print_line(sys.argv[1])
+    print_line(' ' * SPACE_INSIDE)
+    print_line(''.join(blocks[:8]), width)
+    print_line(''.join(blocks[:8]), width)
+    print_line(''.join(blocks[:8]), width)
+    print_line(''.join(blocks[:8]), width)
+    print_line(''.join(blocks[:8]), width)
+    print_line(''.join(blocks[8:]), width)
+    print_line(''.join(blocks[8:]), width)
 
-a = '{magenta}def{reset} {green}self{reset}.example'.format(
-    magenta=Colors.magenta,
-    reset=Colors.reset,
-    green=Colors.green
-)
-b = ("  [{red}'string'{reset}, "
-     '{red}1.0{reset}].each {yellow}do{reset} |{cyan}val{reset}|').format(
-         red=Colors.red,
-         reset=Colors.reset,
-         yellow=Colors.yellow,
-         cyan=Colors.cyan
+    a = '{magenta}def{reset} {green}self{reset}.example'.format(
+        magenta=Colors.magenta,
+        reset=Colors.reset,
+        green=Colors.green
     )
-c = '    puts {magenta}"#{{{reset}val.class{magenta}}}"{reset}'.format(
-    magenta=Colors.magenta,
-    reset=Colors.reset
-)
-d = '  {yellow}end{reset}'.format(yellow=Colors.yellow, reset=Colors.reset)
-e = '{yellow}end{reset}'.format(yellow=Colors.yellow, reset=Colors.reset)
+    b = ("  [{red}'string'{reset}, "
+         '{red}1.0{reset}].each {yellow}do{reset} |{cyan}val{reset}|').format(
+             red=Colors.red,
+             reset=Colors.reset,
+             yellow=Colors.yellow,
+             cyan=Colors.cyan
+        )
+    c = '    puts {magenta}"#{{{reset}val.class{magenta}}}"{reset}'.format(
+        magenta=Colors.magenta,
+        reset=Colors.reset
+    )
+    d = '  {yellow}end{reset}'.format(yellow=Colors.yellow, reset=Colors.reset)
+    e = '{yellow}end{reset}'.format(yellow=Colors.yellow, reset=Colors.reset)
 
 
-print_line(' ' * SPACE_INSIDE)
-print_line(a, 32, 16)
-print_line(b, 32, 31)
-print_line(c, 32, 23)
-print_line(d, 32, 5)
-print_line(e, 32, 3)
-print_line(' ' * SPACE_INSIDE)
-print_line(' ' * SPACE_INSIDE)
-print_line(' ' * SPACE_INSIDE)
-end = """\
+    print_line(' ' * SPACE_INSIDE)
+    print_line(a, 32, 16)
+    print_line(b, 32, 31)
+    print_line(c, 32, 23)
+    print_line(d, 32, 5)
+    print_line(e, 32, 3)
+    print_line(' ' * SPACE_INSIDE)
+    print_line(' ' * SPACE_INSIDE)
+    print_line(' ' * SPACE_INSIDE)
+    end = """\
          ╚══════════════════════════════════════╝\
-"""
-print(end)
+    """
+    print(end)
